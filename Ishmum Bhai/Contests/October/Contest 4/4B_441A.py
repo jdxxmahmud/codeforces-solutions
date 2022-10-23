d2922709b8d8d23b262563bcd2e19121068ce389
Junaid Mahmud <junaidmahmud@iut-dhaka.edu>
2022-10-23 18:34:21 +0600

    contest 4

--- Ishmum Bhai/Contests/October/Contest 4/4B_441A.py
+++ Ishmum Bhai/Contests/October/Contest 4/4B_441A.py
@@ -1,10 +1,22 @@
-n, a, b = map(int, input().split())
+from typing import List
 
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
-res = [2] * n
 
-for i in a:
-    res[i - 1] = 1
-        
-print(*res)
+def solution(n: int, a: int, b: int, list1: List[int], list2: List[int]):
+
+    res = [2] * n
+
+    for i in a:
+        res[i - 1] = 1
+
+    print(*res)
+
+
+def take_input():
+    n, a, b = map(int, input().split())
+
+    list1 = list(map(int, input().split()))
+    list2 = list(map(int, input().split()))
+    solution(n, a, b, list1, list2)
+
+
+take_input()
